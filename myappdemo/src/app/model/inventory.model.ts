c9794d680b126748f5c7aa93624fe2a26a1365e4
Raja <Raja@DESKTOP-ECDA0VS>
2020-12-28 21:38:05 +0530

    deleted unused codes

--- myappdemo/src/app/model/inventory.model.ts
+++
@@ -1,9 +0,0 @@
-import { DatePipe } from '@angular/common';
-
-export class Inventory{
-    inventoryId : number;
-    name: string;
-    price: number;
-    quantity: number;
-    createdDate? : Date;
-}
